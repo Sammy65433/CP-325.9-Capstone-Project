aabee3774150f65144e49dcf716ed1ba20fee783
Sammy65433 <samiamlistening@gmail.com>
2026-08-06 10:53:08 -0400

    CP-325-9-Capstone-Project Sports Training Tracker - Navbar 127

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,14 +2,22 @@ import { Link } from 'react-router-dom'
 import '../styles/Navbar.css'
 
 export default function Navbar() {
+    console.log('Navbar rendered')
     return (
-        <nav>
+        <nav className='navbar'>
+            {/* App logo / brand section  */}
+            <div className='navbar-brand'>
+                <Link to="/dashboard">Sports Training Tracker</Link>
+            </div>
+
+            <div className='navbar-links'>
             <Link to="/dashboard">Dashboard</Link>
             <Link to="/sessions">Sessions</Link>
             <Link to="/goals">Goals</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
+            </div>
         </nav>
     )
 }
--- frontend/src/pages/SessionsPage.jsx
+++ frontend/src/pages/SessionsPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import '../styles/SessionsPage.css'
 
 export default function SessionsPage() {
