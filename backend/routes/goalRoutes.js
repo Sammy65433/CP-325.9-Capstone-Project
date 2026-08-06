9f9737e8dfce7e029efb74c0c1f9d3f2db94d65f
Sammy65433 <samiamlistening@gmail.com>
2026-08-06 13:15:30 -0400

    CP-325-9-Capstone-Project Sports Training Tracker - create the controllers and routes for sessions and goals  141

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,2 +1,14 @@
 import express from 'express'
 import { getGoals, createGoal } from '../controllers/goalController.js'
+
+
+
+const router = express.Router()
+
+// Route to get all goals
+router.get('/', getGoals)
+
+// Route to create a new goal
+router.post('/', createGoal)
+
+export default router
