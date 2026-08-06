1f8d90ac72dc7e624fe5751e3fca43cbf415bbce
Sammy65433 <samiamlistening@gmail.com>
2026-08-06 14:29:36 -0400

    CP-325-9-Capstone-Project Sports Training Tracker - Connect backend to frontend goalservice  153

--- frontend/src/pages/GoalsPage.jsx
+++ frontend/src/pages/GoalsPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/GoalsPage.css'
+import { getGoals, createGoal } from '../services/goalService.js'
 
 
 export default function GoalsPage() {
